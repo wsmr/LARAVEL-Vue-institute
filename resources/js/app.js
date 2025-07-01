571453ccd679e25a6e9728cc0427f304b78e6fce
wsmr <w.s.m.rathnasekara@gmail.com>
2025-07-01 11:21:01 +0530

    v2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('course-card', require('./Components/CourseCard.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
+
+
